81d2b78226189656634e851b9932da675a80056c
CkHong <hck1205@daum.net>
2021-08-18 00:46:51 +0900

    installed material lab and added pagination

--- frontend/src/pages/board/list/presenter.tsx
+++ frontend/src/pages/board/list/presenter.tsx
@@ -1,13 +1,37 @@
 import React, { useEffect } from 'react';
 
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+import { Pagination } from '@material-ui/lab';
+
+import styled from '@emotion/styled';
+
 import { handleRouter } from 'lib';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      '& > *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  })
+);
 
 type Props = {};
 
 function Presenter({}: Props) {
+  const classes = useStyles();
+
   useEffect(() => {}, []);
 
-  return <div>BoardList</div>;
+  return (
+    <PaginationWrapper>
+      <Pagination count={10} variant="outlined" shape="rounded" />
+    </PaginationWrapper>
+  );
 }
+
+const PaginationWrapper = styled.div``;
 
 export default Presenter;
